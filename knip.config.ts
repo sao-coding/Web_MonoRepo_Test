7fae1bba02ca3036598aeb7971dd20df28f876b9
sao-coding <jeter.nice@gmail.com>
2025-04-11 20:23:16 +0800

    fix: knip

--- knip.config.ts
+++ knip.config.ts
@@ -1,25 +1,25 @@
 import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
-  ignore: ['**/fixtures/**'],
+  ignore: ['**/fixtures/**', 'packages/ui/**'],
   vitest: {
     config: ['vitest.{config,shared,workspace}.ts']
   },
   ignoreDependencies: [
     'prettier-plugin-*',
-    'sharp',
+    // 'sharp',
     // TailwindCSS v4 is not detectable currently
-    'tailwindcss'
+    // 'tailwindcss'
   ],
   workspaces: {
     'packages/eslint-config': {
       // @see https://github.com/francoismassart/eslint-plugin-tailwindcss/issues/325
       ignoreDependencies: ['@eslint/config-inspector', 'eslint-plugin-tailwindcss']
     },
-    'packages/ui': {
-      // @see https://github.com/shadcn-ui/ui/issues/4792
-      ignoreDependencies: ['@radix-ui/react-context', '@tailwindcss/typography']
-    }
+    // 'packages/ui': {
+    //   // @see https://github.com/shadcn-ui/ui/issues/4792
+    //   ignoreDependencies: ['@radix-ui/react-context', '@tailwindcss/typography']
+    // }
   }
 }
 
