e24d439d60c3e504f9c4ebc055b4913d5366cfc0
jeterluan <jeterluan@msi.com>
2025-12-04 16:52:41 +0800

    feat: 新增 ESLint 配置的 glob 和 Prettier 設定，並移除不必要的檔案

---
+++ packages/eslint-config/src/backup/globs.ts
@@ -0,0 +1,26 @@
+export const GLOB_SRC_EXT = '?([cm])[jt]s?(x)'
+export const GLOB_SRC = `**/*.${GLOB_SRC_EXT}`
+
+export const GLOB_JS = '**/*.?([cm])js'
+export const GLOB_JSX = '**/*.?([cm])jsx'
+
+export const GLOB_TS = '**/*.?([cm])ts'
+export const GLOB_TSX = '**/*.?([cm])tsx'
+
+export const GLOB_STYLE = '**/*.{c,le,sc}ss'
+export const GLOB_CSS = '**/*.css'
+export const GLOB_POSTCSS = '**/*.{p,post}css'
+export const GLOB_LESS = '**/*.less'
+export const GLOB_SCSS = '**/*.scss'
+
+export const GLOB_JSON = '**/*.json'
+export const GLOB_JSON5 = '**/*.json5'
+export const GLOB_JSONC = '**/*.jsonc'
+
+export const GLOB_MARKDOWN = '**/*.md'
+export const GLOB_YAML = '**/*.y?(a)ml'
+export const GLOB_TOML = '**/*.toml'
+export const GLOB_XML = '**/*.xml'
+export const GLOB_SVG = '**/*.svg'
+export const GLOB_HTML = '**/*.htm?(l)'
+export const GLOB_GRAPHQL = '**/*.{g,graph}ql'
