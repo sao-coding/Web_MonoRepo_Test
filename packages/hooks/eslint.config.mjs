7a4b903009f169e72d7754ef2750b8005d3cfadf
jeterluan <jeterluan@msi.com>
2025-12-04 17:29:46 +0800

    refactor: 更新 ESLint 配置以使用 defineConfig，移除不必要的設定

--- packages/hooks/eslint.config.mjs
+++ packages/hooks/eslint.config.mjs
@@ -1,7 +1,3 @@
-import msi from '@msi/eslint-config'
+import { defineConfig } from "@msi/eslint-config";
 
-export default msi({
-  project: './tsconfig.json',
-  tsconfigRootDir: import.meta.dirname,
-  turbo: true
-})
+export default defineConfig()
