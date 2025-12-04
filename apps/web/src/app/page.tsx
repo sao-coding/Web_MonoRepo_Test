21e578e5a824b41b1ec7560713a1ccd5a394882b
jeterluan <jeterluan@msi.com>
2025-12-04 16:59:16 +0800

    refactor: 簡化 ESLint 配置並修正格式化問題

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,14 @@
-import { Button } from '@msi/ui/components/button'
+import { Button } from "@msi/ui/components/button";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Button>Click Me</Button>
+      <Link href="/about">Go to About Page</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
