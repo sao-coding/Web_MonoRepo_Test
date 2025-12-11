0da9feb81138fd741c1344855c727c35246ef6c4
sao-coding <jeter.nice@gmail.com>
2025-12-12 01:39:08 +0800

    refactor: 重構Collapsible和Slider組件以使用函數式組件，並統一引入語法

--- packages/ui/src/components/slider.tsx
+++ packages/ui/src/components/slider.tsx
@@ -1,9 +1,8 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import * as SliderPrimitive from "@radix-ui/react-slider"
-
-import { cn } from "@msi/ui/lib/utils"
+import { cn } from '@msi/ui/lib/utils'
+import * as SliderPrimitive from '@radix-ui/react-slider'
+import * as React from 'react'
 
 function Slider({
   className,
@@ -25,31 +24,35 @@ function Slider({
 
   return (
     <SliderPrimitive.Root
-      data-slot="slider"
+      data-slot='slider'
       defaultValue={defaultValue}
       value={value}
       min={min}
       max={max}
       className={cn(
-        "relative flex w-full touch-none items-center select-none data-disabled:opacity-50",
+        'relative flex w-full touch-none items-center select-none data-[disabled]:opacity-50 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col',
         className
       )}
       {...props}
     >
       <SliderPrimitive.Track
-        data-slot="slider-track"
-        className="bg-secondary relative h-2 w-full grow overflow-hidden rounded-full"
+        data-slot='slider-track'
+        className={cn(
+          'bg-muted relative grow overflow-hidden rounded-full data-[orientation=horizontal]:h-1.5 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-1.5'
+        )}
       >
         <SliderPrimitive.Range
-          data-slot="slider-range"
-          className="bg-primary absolute h-full"
+          data-slot='slider-range'
+          className={cn(
+            'bg-primary absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full'
+          )}
         />
       </SliderPrimitive.Track>
       {Array.from({ length: _values.length }, (_, index) => (
         <SliderPrimitive.Thumb
-          data-slot="slider-thumb"
+          data-slot='slider-thumb'
           key={index}
-          className="border-primary bg-background ring-offset-background block h-5 w-5 rounded-full border-2 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+          className='block size-4 shrink-0 rounded-full border border-primary bg-white shadow-sm ring-ring/50 transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50'
         />
       ))}
     </SliderPrimitive.Root>
